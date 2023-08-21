e799d7aab4dfeb2c20af7ef4c02daf63d4e2eb7a
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2023-08-21 19:31:32 +0500

    Add `bs::string_view` constructors

--- include/betterstring.hpp
+++ include/betterstring.hpp
@@ -2,34 +2,76 @@
 
 #include <string>
 #include <iterator>
+#include <type_traits>
+#include <memory>
 
 namespace bs
 {
 
+namespace detail {
+    template<class T, class = void>
+    inline constexpr bool has_pointer_traits_to_address = false;
+    template<class T>
+    inline constexpr bool has_pointer_traits_to_address<T, std::void_t<
+        decltype(std::pointer_traits<T>::to_address(std::declval<T&>()))
+    >> = true;
+
+    template<class T>
+    constexpr T* to_address(T* ptr) noexcept {
+        static_assert(!std::is_function_v<T>);
+    }
+    template<class T>
+    constexpr auto to_address(const T& fancy_ptr) noexcept {
+        if constexpr (has_pointer_traits_to_address<T>) {
+            return std::pointer_traits<T>::to_address(fancy_ptr);
+        } else {
+            return to_address(fancy_ptr.operator->());
+        }
+    }
+}
+
 template<class T>
-struct string_traits {
-    using char_traits = std::char_traits<T>;
-    using value_type = T;
+struct char_traits : std::char_traits<T> {
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
 };
 
-template<class Traits = string_traits<char>>
+template<class Traits = char_traits<char>>
 class string_view {
 public:
-    using traits_type = Traits;
-    using value_type = typename Traits::value_type;
-    using pointer = value_type*;
-    using const_pointer = const value_type*;
-    using reference = value_type&;
-    using const_reference = const value_type&;
-    using const_iterator = const value_type*;
-    using iterator = const_iterator;
+    using traits_type            = Traits;
+    using value_type             = typename Traits::char_type;
+    using pointer                = value_type*;
+    using const_pointer          = const value_type*;
+    using reference              = value_type&;
+    using const_reference        = const value_type&;
+    using const_iterator         = const value_type*;
+    using iterator               = const_iterator;
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
-    using reverse_iterator = const_reverse_iterator;
-    using size_type = typename Traits::size_type;
-    using difference_type = typename Traits::difference_type;
+    using reverse_iterator       = const_reverse_iterator;
+    using size_type              = typename Traits::size_type;
+    using difference_type        = typename Traits::difference_type;
+
+    constexpr string_view() noexcept : string_data(nullptr), string_size(0) {}
+
+    constexpr string_view(const string_view) noexcept = default;
+
+    constexpr string_view(const const_pointer str, const size_type count)
+        : string_data(str), string_size(count) {}
+
+    constexpr string_view(const const_pointer ntstr) noexcept
+        : string_data(ntstr), string_size(traits_type::length(ntstr)) {}
+
+    template<class Begin, class End = Begin>
+    constexpr string_view(Begin first, End last) noexcept(noexcept(last - first))
+        : string_data(detail::to_address(first))
+        , string_size(static_cast<size_type>(last - first)) {}
+
+    constexpr string_view(std::nullptr_t) = delete;
 
 private:
-
+    const_pointer string_data;
+    const_pointer string_size;
 };
 
 }
