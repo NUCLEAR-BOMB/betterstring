2f493b30ff0a40017d4cc8c0ba686e3f9e870ebd
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2023-08-24 23:31:42 +0500

    Add `strstr`, `strfind_string` benchmarks

--- benchmark/functions.cpp
+++ benchmark/functions.cpp
@@ -11,9 +11,10 @@ static void printable_chars_args(benchmark::internal::Benchmark* const b) {
     }
 }
 
+// MSVC:
 // mean 40.609052631579 ns
-// max 165 ns
-// min 1.91 ns
+// max  165 ns
+// min  1.91 ns
 BENCHMARK_DEFINE_F(string_fixture, strrfind)(benchmark::State& state) {
     const auto string = get_string();
     const auto size = get_string_size();
@@ -23,9 +24,10 @@ BENCHMARK_DEFINE_F(string_fixture, strrfind)(benchmark::State& state) {
 }
 BENCHMARK_REGISTER_F(string_fixture, strrfind)->Apply(printable_chars_args);
 
+// MSVC:
 // mean 615.36842105263 ns
-// max 679 ns
-// min 576 ns
+// max  679 ns
+// min  576 ns
 BENCHMARK_DEFINE_F(string_fixture, strrchr)(benchmark::State& state) {
     const auto string = get_string();
     for (auto _ : state) {
@@ -34,13 +36,45 @@ BENCHMARK_DEFINE_F(string_fixture, strrchr)(benchmark::State& state) {
 }
 BENCHMARK_REGISTER_F(string_fixture, strrchr)->Apply(printable_chars_args);
 
-BENCHMARK_F(string_fixture, strfind)(benchmark::State& state) {
-    const auto string = get_string();
-    const auto size = get_string_size();
-    for (auto _ : state) {
-        benchmark::DoNotOptimize(bs::strfind(string, size, char(state.range())));
+
+static void strings_args(benchmark::internal::Benchmark* const b) {
+    for (std::int64_t i = 1; i <= 20; ++i) {
+        b->Arg(i * 5653206889367LL);
     }
 }
-BENCHMARK_REGISTER_F(string_fixture, strfind)->Apply(printable_chars_args);
+
+// MSVC:
+// mean 141.5 ns
+// max 	851 ns
+// min  139 ns
+BENCHMARK_DEFINE_F(string_fixture, strfind_string)(benchmark::State& state) {
+    const int64_t integer = state.range();
+    const char* const needle = reinterpret_cast<const char*>(&integer);
+    const char* const haystack = get_string();
+    const size_t haystack_len = get_string_size();
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(
+            bs::strfind(haystack, haystack_len, needle, sizeof(integer))
+        );
+    }
+}
+BENCHMARK_REGISTER_F(string_fixture, strfind_string)->Apply(strings_args);
+
+// MSVC:
+// mean 506.5 ns
+// max  600 ns
+// min  501 ns
+BENCHMARK_DEFINE_F(string_fixture, strstr)(benchmark::State& state) {
+    const int64_t integer[2] = { state.range(), '\0' };
+    const char* const needle = reinterpret_cast<const char*>(&integer);
+    const char* const haystack = get_string();
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(
+            std::strstr(haystack, needle)
+        );
+    }
+}
+BENCHMARK_REGISTER_F(string_fixture, strstr)->Apply(strings_args);
+
 
 }
