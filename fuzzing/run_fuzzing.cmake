f2f8a2191849686029c4ce3dec294a612d1f2404
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2023-09-02 18:42:14 +0500

    Print executable path on fuzzing

--- fuzzing/run_fuzzing.cmake
+++ fuzzing/run_fuzzing.cmake
@@ -1,10 +1,10 @@
 
-message(STATUS "Running fuzzing")
+message(STATUS "Running '${executable}' fuzzing")
 
 execute_process(
     COMMAND ${executable} -fork=${forks} -runs=${runs}
     RESULTS_VARIABLE result
 )
 if (NOT result STREQUAL 0)
-    message(FATAL_ERROR "Fuzzing failed")
+    message(FATAL_ERROR "Fuzzing '${executable}' failed")
 endif()
