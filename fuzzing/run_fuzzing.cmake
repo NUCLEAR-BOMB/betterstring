0c7e443d2aa16d101e75c223b4762047fee74929
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2023-09-07 19:57:44 +0500

    Add a success message

--- fuzzing/run_fuzzing.cmake
+++ fuzzing/run_fuzzing.cmake
@@ -1,5 +1,5 @@
 
-message(STATUS "Running '${executable}' fuzzing")
+message(STATUS "Running fuzzing: '${executable}'")
 
 execute_process(
     COMMAND ${executable} -fork=${forks} -max_total_time=${time} -print_final_stats=1 -print_pcs=1
@@ -7,4 +7,6 @@ execute_process(
 )
 if (NOT result STREQUAL 0)
     message(FATAL_ERROR "Fuzzing '${executable}' failed with code ${result}")
+else()
+    message(STATUS "Fuzzing '${executable}' completed successfully")
 endif()
