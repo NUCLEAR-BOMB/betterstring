8d4ada78c79618770bd3736668200e338f7eadee
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2023-12-30 00:50:27 +0500

    Improve performance of `strrfind_char_default`

--- include/betterstring/detail/multiarch/strrfind_char_default.hpp
+++ include/betterstring/detail/multiarch/strrfind_char_default.hpp
@@ -4,10 +4,13 @@
 #include <cstring>
 
 namespace bs::detail::multiarch {
-    inline const char* strrfind_char_default(const char* const str, const std::size_t count, const char ch) {
-        for (std::size_t i = count; i > 0; --i) {
-            if (str[i - 1] == ch) return str + i - 1;
-        }
+    inline const char* strrfind_char_default(const char* str, std::size_t count, const char ch) {
+        if (count == 0) return nullptr;
+        str += count;
+        do {
+            --str;
+            if (*str == ch) return str;
+        } while (--count != 0);
         return nullptr;
     }
 
