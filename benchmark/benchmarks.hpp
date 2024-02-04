f8624753e6531a7c8f7d92c011799e0c65743029
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2024-02-04 15:00:48 +0500

    Add more benchmark samples

--- benchmark/benchmarks.hpp
+++ benchmark/benchmarks.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <array>
+#include <utility>
+#include <type_traits>
 #include <nanobench.h>
 
 #include <betterstring/functions.hpp>
@@ -8,6 +11,17 @@
 #include <fmt/format.h>
 
 #include "samples.hpp"
+
+template<class T, std::size_t N, std::size_t... I>
+constexpr auto to_array_impl(T (&&a)[N], std::index_sequence<I...>)
+    -> std::array<std::remove_cv_t<T>, N> {
+    return {{std::move(a[I])...}};
+}
+ 
+template<class T, std::size_t N>
+constexpr std::array<std::remove_cv_t<T>, N> to_array(T (&&a)[N]) {
+    return to_array_impl(std::move(a), std::make_index_sequence<N>{});
+}
 
 inline void benchmark_strrfind_string(ankerl::nanobench::Bench& bench) {
     using ankerl::nanobench::doNotOptimizeAway;
@@ -31,26 +45,69 @@ inline void benchmark_strrfind_string(ankerl::nanobench::Bench& bench) {
 
 inline void benchmark_strrfind_character(ankerl::nanobench::Bench& bench) {
     using ankerl::nanobench::doNotOptimizeAway;
-    bench.minEpochIterations(450'000).run("character (229362)", []() BS_NOINLINE {
+    
+    const auto samples = to_array<std::size_t>({
+        229363,
+        114681,
+        80000,
+        40000,
+        20000,
+        9000,
+        8192,
+        4200,
+        4096,
+        2048,
+        1024,
+        512,
+        384,
+        256,
+        192,
+        128,
+        96,
+        64,
+        48,
+        32,
+        20,
+    });
+    using namespace std::chrono_literals;
+    bench.minEpochTime(200ms).minEpochIterations(500);
+    for (const auto& sample : samples) {
+        bench.run(fmt::format("character ({} lenght)", sample), [&]() {
+            auto result = bs::strrfind(strings::HOMOGENEOUS, sample, 'b');
+            doNotOptimizeAway(result);
+        });
+    }
+
+#if 0
+    bench.minEpochIterations(320'000).run("character (229362)", []() BS_NOINLINE {
         auto result = bs::strrfind(strings::HOMOGENEOUS, 229362, 'b');
         doNotOptimizeAway(result);
     });
-    bench.minEpochIterations(500'000).run("character (114681)", []() BS_NOINLINE {
+    bench.minEpochIterations(400'000).run("character (114681)", []() BS_NOINLINE {
         auto result = bs::strrfind(strings::HOMOGENEOUS, 114681, 'b');
         doNotOptimizeAway(result);
     });
-    bench.minEpochIterations(65'000'000).run("character (1000)", []() BS_NOINLINE {
+    bench.minEpochIterations(60'000'000).run("character (1000)", []() BS_NOINLINE {
         auto result = bs::strrfind(strings::HOMOGENEOUS, 1000, 'b');
         doNotOptimizeAway(result);
     });
-    bench.minEpochIterations(100'000'000).run("character (250)", []() BS_NOINLINE {
+    bench.minEpochIterations(80'000'000).run("character (250)", []() BS_NOINLINE {
         auto result = bs::strrfind(strings::HOMOGENEOUS, 250, 'b');
         doNotOptimizeAway(result);
     });
-    bench.minEpochIterations(500'000'000).run("character (60)", []() BS_NOINLINE {
+    bench.minEpochIterations(110'000'000).run("character (60)", []() BS_NOINLINE {
         auto result = bs::strrfind(strings::HOMOGENEOUS, 60, 'b');
         doNotOptimizeAway(result);
     });
+    bench.minEpochIterations(200'000'000).run("character (32)", []() BS_NOINLINE {
+        auto result = bs::strrfind(strings::HOMOGENEOUS, 32, 'b');
+        doNotOptimizeAway(result);
+    });
+    bench.minEpochIterations(200'000'000).run("character (20)", []() BS_NOINLINE {
+        auto result = bs::strrfind(strings::HOMOGENEOUS, 20, 'b');
+        doNotOptimizeAway(result);
+    });
+#endif
 }
 
 inline void benchmark_parse_u8(ankerl::nanobench::Bench& bench) {
--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -13,9 +13,12 @@
 
 #include <fmt/format.h>
 
+#include <betterstring/parsing.hpp>
+#include <betterstring/string_view.hpp>
+
 namespace nb = ankerl::nanobench;
 
-int main() {
+int main(int argc, char* argv[]) {
 #ifdef _WIN32
     if (!SetPriorityClass(GetCurrentProcess(), REALTIME_PRIORITY_CLASS)) {
         fmt::println("warning: failed to set process priority class");
@@ -25,12 +28,29 @@ int main() {
     }
 #endif // _WIN32
 
+    if (argc <= 1) {
+        fmt::println("select the name benchmark: "
+            "[strrfind_ch,strrfind_str,parse_u8,parse_u16,parse_u32,parse_u64]");
+        return -1;
+    }
     nb::Bench bench;
 
-    // benchmark_strrfind_character(bench);
-    // benchmark_strrfind_string(bench);
-    // benchmark_parse_u8(bench);
-    // benchmark_parse_u16(bench);
-    // benchmark_parse_u32(bench);
-    // benchmark_parse_u64(bench);
+    const bs::string_view benchmark_name{argv[1], bs::strlen(argv[1])};
+    if (benchmark_name == "strrfind_ch") {
+        benchmark_strrfind_character(bench);
+    } else if (benchmark_name == "strrfind_str") {
+        benchmark_strrfind_string(bench);
+    } else if (benchmark_name == "parse_u8") {
+        benchmark_parse_u8(bench);
+    } else if (benchmark_name == "parse_u16") {
+        benchmark_parse_u16(bench);
+    } else if (benchmark_name == "parse_u32") {
+        benchmark_parse_u32(bench);
+    } else if (benchmark_name == "parse_u64") {
+        benchmark_parse_u64(bench);
+    } else {
+        fmt::println("unknown benchmark name: {}", benchmark_name.data());
+        return -1;
+    }
+    return 0;
 }
