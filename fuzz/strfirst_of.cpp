8c5c79059a4a028ba0a29356f01f102f426e32a9
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2024-02-23 13:55:07 +0500

    Add fuzzer for `bs::strfirst_of` function

---
+++ fuzz/strfirst_of.cpp
@@ -0,0 +1,34 @@
+#include <cinttypes>
+#include <cassert>
+#include <new>
+
+#include <betterstring/functions.hpp>
+#include <algorithm>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size) {
+    if (Size == 0) { return -1; }
+
+    const size_t needle_len = (Data[0] / 2) <= Size ? (Data[0] / 2) : Size;
+    // if (needle_len > 2) { return -1; }
+    // if (needle_len != 4) { return -1; }
+
+    const char* const needle = reinterpret_cast<const char*>(Data);
+
+    const size_t str_len = Size - needle_len;
+    const char* const str = reinterpret_cast<const char*>(Data + needle_len);
+
+    const char* const result = bs::strfirst_of(str, str_len, needle, needle_len);
+
+    const char* const true_result = std::find_first_of(str, str + str_len, needle, needle + needle_len);
+    if (true_result == str + str_len) {
+        if (result != nullptr) {
+            std::abort();
+        }
+    } else {
+        if (result != true_result) {
+            std::abort();
+        }
+    }
+
+    return 0;
+}
