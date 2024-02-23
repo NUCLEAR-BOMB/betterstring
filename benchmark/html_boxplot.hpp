8987bd40d20f67a855d78140aa6f27f14c43e48e
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2024-02-23 13:57:02 +0500

    Add `name:` in html template

--- benchmark/html_boxplot.hpp
+++ benchmark/html_boxplot.hpp
@@ -4,5 +4,6 @@ inline constexpr char html_boxplot_template[] = R"TEMPLATE(
 {
     x: [{{#result}}{{context(length)}},{{/result}}],
     y: [{{#result}}{{average(elapsed)}},{{/result}}],
+    name: '{{title}}',
 },
 )TEMPLATE";
