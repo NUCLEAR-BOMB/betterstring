379a836bf99ce23ea7753bea8077f7f71752856f
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2024-02-26 22:59:46 +0500

    Add constant variable `max_needle_len`

--- fuzz/strfirst_of.cpp
+++ fuzz/strfirst_of.cpp
@@ -5,14 +5,17 @@
 #include <betterstring/functions.hpp>
 #include <algorithm>
 
+static constexpr uint8_t max_needle_len = 6;
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size) {
     if (Size == 0) { return -1; }
 
-    const size_t needle_len = (Data[0] % 5) <= Size ? (Data[0] % 5) : Size;
+    const size_t needle_len = (Data[0] % (max_needle_len + 1)) <= Size ? (Data[0] % (max_needle_len + 1)) : Size;
 
     const char* const needle = reinterpret_cast<const char*>(Data);
 
     const size_t str_len = Size - needle_len;
+
     const char* const str = reinterpret_cast<const char*>(Data + needle_len);
 
     const char* const result = bs::strfirst_of(str, str_len, needle, needle_len);
