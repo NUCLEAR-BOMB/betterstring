25516246ad9bcbc9af56348eb16c56a33d6d531f
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2024-03-05 20:53:12 +0500

    Improve `bs::parse` unsigned integer fuzzer coverage

--- fuzz/parse.cpp
+++ fuzz/parse.cpp
@@ -12,9 +12,6 @@ void parse_fuzz(const char* const str, const std::size_t count) {
     T fc_res = 0;
     const std::from_chars_result fc_err = std::from_chars(str, str + count, fc_res);
 
-    if (res.error() == bs::parse_error::too_long) {
-        return;
-    }
     if (fc_err.ec == std::errc::invalid_argument) {
         if (res.error() != bs::parse_error::invalid_argument) {
             TRAP();
@@ -44,14 +41,31 @@ void parse_fuzz(const char* const str, const std::size_t count) {
 }
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size) {
-    if (Size >= 25) return -1;
+    if (Size > 30) { return -1; }
 
-    const char* const str = reinterpret_cast<const char*>(Data);
+    const char* str = reinterpret_cast<const char*>(Data);
+    std::size_t str_len = Size;
 
-    parse_fuzz<std::uint8_t>(str, Size);
-    parse_fuzz<std::uint16_t>(str, Size);
-    parse_fuzz<std::uint32_t>(str, Size);
-    parse_fuzz<std::uint64_t>(str, Size);
+    uint64_t fc_res = 0;
+    const auto [ptr, err] = std::from_chars(str, str + str_len, fc_res);
+    if (err != std::errc{} || ptr != (str + str_len)) { return -1; }
+
+    for (; str_len > 0; ++str, --str_len) {
+        if (*str != '0') { break; }
+    }
+
+    if (str_len <= 20) {
+        parse_fuzz<std::uint64_t>(str, str_len);
+    }
+    if (str_len <= 10) {
+        parse_fuzz<std::uint32_t>(str, str_len);
+    }
+    if (str_len <= 5) {
+        parse_fuzz<std::uint16_t>(str, str_len);
+    }
+    if (str_len <= 3) {
+        parse_fuzz<std::uint8_t>(str, str_len);
+    }
 
     return 0;
 }
