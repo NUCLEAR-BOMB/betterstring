a17b9ebcdce9ca90f76035557360ab99ca2dd302
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2024-03-06 21:01:35 +0500

    Add 'safe' functions

---
+++ include/betterstring/safe_functions.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <betterstring/string_view.hpp>
+#include <betterstring/functions.hpp>
+
+namespace bs::safe {
+
+namespace detail {
+    template<class T>
+    constexpr bool is_overlap(const T* first, std::size_t first_len, const T* second, std::size_t second_len) noexcept {
+        return (first < second + second_len) && (second < first + first_len);
+    }
+}
+
+enum class errorc {
+    null_pointer      = 1,
+    dest_null_pointer = 2,
+    src_null_pointer  = 3,
+    not_enough_space  = 4,
+    overlaping        = 5,
+};
+
+class error {
+    errorc error_code;
+public:
+    constexpr error(const errorc err) noexcept : error_code{err} {}
+
+    constexpr errorc code() const noexcept { return error_code; }
+
+    constexpr bs::string_view error_message() const noexcept {
+        BS_VERIFY(error_code != errorc{}, "no error code");
+
+        switch (error_code) {
+        case errorc::null_pointer: return "pointer operand is null pointer with non-zero size";
+        case errorc::dest_null_pointer: return "destination operand is null pointer with non-zero size";
+        case errorc::src_null_pointer: return "source operand is null pointer with non-zero size";
+        case errorc::not_enough_space: return "source buffer length is exceeding destination buffer";
+        case errorc::overlaping: return "two memory buffers are overlaping";
+        }
+        BS_UNREACHABLE();
+    }
+
+    explicit constexpr operator bool() const noexcept {
+        return error_code != errorc{};
+    }
+
+    friend constexpr bool operator==(const error left, const errorc right) noexcept {
+        return left.error_code == right;
+    }
+    friend constexpr bool operator==(const errorc left, const error right) noexcept {
+        return left == right.error_code;
+    }
+    friend constexpr bool operator!=(const error left, const errorc right) noexcept {
+        return left.error_code != right;
+    }
+    friend constexpr bool operator!=(const errorc left, const error right) noexcept {
+        return left != right.error_code;
+    }
+};
+
+template<class T>
+constexpr error strcopy(T* const dest, const std::size_t dest_len, const T* const src, const std::size_t count) noexcept {
+    if (dest == nullptr && dest_len != 0) { return errorc::dest_null_pointer; }
+    if (src == nullptr && count != 0) { return errorc::src_null_pointer; }
+    if (count > dest_len) { return errorc::not_enough_space; }
+
+    if (dest == nullptr || src == nullptr) { return errorc{}; }
+
+    if (detail::is_overlap(dest, dest_len, src, count)) {
+        return errorc::overlaping;
+    }
+
+    bs::strcopy(dest, src, count);
+
+    return errorc{};
+}
+
+template<class T>
+constexpr error strmove(T* const dest, const std::size_t dest_len, const T* const src, const std::size_t count) noexcept {
+    if (dest == nullptr && dest_len != 0) { return errorc::dest_null_pointer; }
+    if (src == nullptr && count != 0) { return errorc::src_null_pointer; }
+    if (count > dest_len) { return errorc::not_enough_space; }
+
+    if (dest == nullptr || src == nullptr) { return errorc{}; }
+
+    bs::strmove(dest, src, count);
+
+    return errorc{};
+}
+
+}
---
+++ test/safe_functions.cpp
@@ -0,0 +1,79 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <betterstring/safe_functions.hpp>
+
+namespace Catch {
+    template<>
+    struct StringMaker<bs::safe::error> {
+        static std::string convert(const bs::safe::error value) {
+            return std::string{value.error_message()};
+        }
+    };
+    template<>
+    struct StringMaker<bs::safe::errorc> {
+        static std::string convert(const bs::safe::errorc value) {
+            return std::to_string(static_cast<uint32_t>(value));
+        }
+    };
+}
+
+TEST_CASE("bs::safe::strcopy", "[safe functions]") {
+    char* const char_nullptr = nullptr;
+
+    char buffer[20]{};
+    auto err1 = bs::safe::strcopy(buffer, 20, "hello world", 11);
+    CHECK(err1 == bs::safe::errorc{});
+
+    auto err2 = bs::safe::strcopy(buffer, 20, "long long long long string", 26);
+    CHECK(err2 == bs::safe::errorc::not_enough_space);
+    CHECK(err2.error_message() != "");
+
+    auto err3 = bs::safe::strcopy(char_nullptr, 0, char_nullptr, 0);
+    CHECK(err3 == bs::safe::errorc{});
+
+    auto err4 = bs::safe::strcopy(buffer, 20, char_nullptr, 0);
+    CHECK(err4 == bs::safe::errorc{});
+
+    auto err5 = bs::safe::strcopy(buffer, 20, char_nullptr, 1);
+    CHECK(err5 == bs::safe::errorc::src_null_pointer);
+    CHECK(err5.error_message() != "");
+
+    auto err6 = bs::safe::strcopy(char_nullptr, 1, "test", 4);
+    CHECK(err6 == bs::safe::errorc::dest_null_pointer);
+    CHECK(err6.error_message() != "");
+
+    auto err7 = bs::safe::strcopy(buffer, 20, buffer + 5, 5);
+    CHECK(err7 == bs::safe::errorc::overlaping);
+
+    auto err8 = bs::safe::strcopy(buffer, 10, buffer + 10, 10);
+    CHECK(err8 == bs::safe::errorc{});
+}
+
+TEST_CASE("bs::safe::strmove", "[safe functions]") {
+    char* const char_nullptr = nullptr;
+
+    char buffer[20]{};
+    auto err1 = bs::safe::strmove(buffer, 20, "hello world", 11);
+    CHECK(err1 == bs::safe::errorc{});
+
+    auto err2 = bs::safe::strmove(buffer, 20, "long long long long string", 26);
+    CHECK(err2 == bs::safe::errorc::not_enough_space);
+    CHECK(err2.error_message() != "");
+
+    auto err3 = bs::safe::strmove(char_nullptr, 0, char_nullptr, 0);
+    CHECK(err3 == bs::safe::errorc{});
+
+    auto err4 = bs::safe::strmove(buffer, 20, char_nullptr, 0);
+    CHECK(err4 == bs::safe::errorc{});
+
+    auto err5 = bs::safe::strmove(buffer, 20, char_nullptr, 1);
+    CHECK(err5 == bs::safe::errorc::src_null_pointer);
+    CHECK(err5.error_message() != "");
+
+    auto err6 = bs::safe::strmove(char_nullptr, 1, "test", 4);
+    CHECK(err6 == bs::safe::errorc::dest_null_pointer);
+    CHECK(err6.error_message() != "");
+
+    auto err8 = bs::safe::strmove(buffer, 20, buffer + 10, 10);
+    CHECK(err8 == bs::safe::errorc{});
+}
