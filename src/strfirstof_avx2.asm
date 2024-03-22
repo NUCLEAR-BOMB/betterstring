cabbf0dc4c115dbe7431b89aee3f3d2c00c85300
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2024-03-22 22:35:57 +0500

    Add licence and licence headers

--- src/strfirstof_avx2.asm
+++ src/strfirstof_avx2.asm
@@ -1,4 +1,8 @@
 
+; // Copyright 2024.
+; // Distributed under the Boost Software License, Version 1.0.
+; // (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+
 PAGE_SIZE equ 4096
 
 PUSH_XMM MACRO regs:VARARG
