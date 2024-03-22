c9512000f80fd09db43c5fd8d0341fcb4983f26d
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2024-03-23 02:09:50 +0500

    Print used instructions

--- tools/view-cpu-features.py
+++ tools/view-cpu-features.py
@@ -12,7 +12,7 @@ CPU_FEATURES_TABLE = {
     "POPCNT": ("POPCNT", ),
     "LZCNT": ("LZCNT", ),
     "AVX2": ("vpabsw", "vpabsd", "vpabsb", "vpaddw", "vpaddd", "vpaddq", "vpaddb", "vpaddsw", "vpaddsb", "vpaddusw", "vpaddusb", "vpalignr", "vpand", "vpandn", "vpavgw", "vpavgb", "vpblendw", "vpblendd", "vpblendd", "vpblendvb", "vpbroadcastb", "vpbroadcastb", "vpbroadcastd", "vpbroadcastd", "vpbroadcastq", "vpbroadcastq", "movddup", "vbroadcastsd", "vbroadcasti128", "vbroadcasti128", "vbroadcastss", "vbroadcastss", "vpbroadcastw", "vpbroadcastw", "vpslldq", "vpsrldq", "vpcmpeqw", "vpcmpeqd", "vpcmpeqq", "vpcmpeqb", "vpcmpgtw", "vpcmpgtd", "vpcmpgtq", "vpcmpgtb", "vpmovsxwd", "vpmovsxwq", "vpmovsxdq", "vpmovsxbw", "vpmovsxbd", "vpmovsxbq", "vpmovzxwd", "vpmovzxwq", "vpmovzxdq", "vpmovzxbw", "vpmovzxbd", "vpmovzxbq", "vextracti128", "vphaddw", "vphaddd", "vphaddsw", "vphsubw", "vphsubd", "vphsubsw", "vpgatherdd", "vpgatherdd", "vpgatherdd", "vpgatherdd", "vpgatherdq", "vpgatherdq", "vpgatherdq", "vpgatherdq", "vgatherdpd", "vgatherdpd", "vgatherdpd", "vgatherdpd", "vgatherdps", "vgatherdps", "vgatherdps", "vgatherdps", "vpgatherqd", "vpgatherqd", "vpgatherqd", "vpgatherqd", "vpgatherqq", "vpgatherqq", "vpgatherqq", "vpgatherqq", "vgatherqpd", "vgatherqpd", "vgatherqpd", "vgatherqpd", "vgatherqps", "vgatherqps", "vgatherqps", "vgatherqps", "vinserti128", "vpmaddwd", "vpmaddubsw", "vpmaskmovd", "vpmaskmovd", "vpmaskmovq", "vpmaskmovq", "vpmaskmovd", "vpmaskmovd", "vpmaskmovq", "vpmaskmovq", "vpmaxsw", "vpmaxsd", "vpmaxsb", "vpmaxuw", "vpmaxud", "vpmaxub", "vpminsw", "vpminsd", "vpminsb", "vpminuw", "vpminud", "vpminub", "vpmovmskb", "vmpsadbw", "vpmuldq", "vpmuludq", "vpmulhw", "vpmulhuw", "vpmulhrsw", "vpmullw", "vpmulld", "vpor", "vpacksswb", "vpackssdw", "vpackuswb", "vpackusdw", "vperm2i128", "vpermq", "vpermpd", "vpermd", "vpermps", "vpsadbw", "vpshufd", "vpshufb", "vpshufhw", "vpshuflw", "vpsignw", "vpsignd", "vpsignb", "vpsllw", "vpslld", "vpsllq", "vpsllw", "vpslld", "vpsllq", "vpslldq", "vpsllvd", "vpsllvd", "vpsllvq", "vpsllvq", "vpsraw", "vpsrad", "vpsraw", "vpsrad", "vpsravd", "vpsravd", "vpsrlw", "vpsrld", "vpsrlq", "vpsrlw", "vpsrld", "vpsrlq", "vpsrldq", "vpsrlvd", "vpsrlvd", "vpsrlvq", "vpsrlvq", "vmovntdqa", "vpsubw", "vpsubd", "vpsubq", "vpsubb", "vpsubsw", "vpsubsb", "vpsubusw", "vpsubusb", "vpunpckhwd", "vpunpckhdq", "vpunpckhqdq", "vpunpckhbw", "vpunpcklwd", "vpunpckldq", "vpunpcklqdq", "vpunpcklbw", "vpxor"),
-    "AVX": ("vaddpd", "vaddps", "vaddsubpd", "vaddsubps", "vandpd", "vandps", "vandnpd", "vandnps", "vblendpd", "vblendps", "vblendvpd", "vblendvps", "vbroadcastf128", "vbroadcastf128", "vbroadcastsd", "vbroadcastss", "vbroadcastss", "vroundpd", "vroundps", "vcmppd", "vcmppd", "vcmpps", "vcmpps", "vcmpsd", "vcmpss", "vcvtdq2pd", "vcvtdq2ps", "vcvtpd2dq", "vcvtpd2ps", "vcvtps2dq", "vcvtps2pd", "vmovsd", "vmovd", "vmovss", "vcvttpd2dq", "vcvttps2dq", "vdivpd", "vdivps", "vdpps", "vextractf128", "vextractf128", "vextractf128", "vroundpd", "vroundps", "vhaddpd", "vhaddps", "vhsubpd", "vhsubps", "vinsertf128", "vinsertf128", "vinsertf128", "vlddqu", "vmovapd", "vmovaps", "vmovdqa", "vmovupd", "vmovups", "vmovdqu", "vmaskmovpd", "vmaskmovpd", "vmaskmovps", "vmaskmovps", "vmaskmovpd", "vmaskmovpd", "vmaskmovps", "vmaskmovps", "vmaxpd", "vmaxps", "vminpd", "vminps", "vmovddup", "vmovshdup", "vmovsldup", "vmovmskpd", "vmovmskps", "vmulpd", "vmulps", "vorpd", "vorps", "vpermilpd", "vpermilpd", "vpermilps", "vpermilps", "vperm2f128", "vperm2f128", "vperm2f128", "vpermilpd", "vpermilpd", "vpermilps", "vpermilps", "vrcpps", "vroundpd", "vroundps", "vrsqrtps", "vinsertf128", "vinsertf128", "vinsertf128", "vinsertf128", "vinsertf128", "vinsertf128", "vxorpd", "vxorps", "vpxor", "vshufpd", "vshufps", "vsqrtpd", "vsqrtps", "vmovapd", "vmovaps", "vmovdqa", "vmovupd", "vmovups", "vmovdqu", "vmovntpd", "vmovntps", "vmovntdq", "vsubpd", "vsubps", "vtestpd", "vtestpd", "vtestps", "vtestps", "vptest", "vtestpd", "vtestpd", "vtestps", "vtestps", "vptest", "vtestpd", "vtestpd", "vtestps", "vtestps", "vptest", "vunpckhpd", "vunpckhps", "vunpcklpd", "vunpcklps", "vxorpd", "vxorps", "vzeroall", "vzeroupper"),
+    "AVX": ("vaddpd", "vaddps", "vaddsubpd", "vaddsubps", "vandpd", "vandps", "vandnpd", "vandnps", "vblendpd", "vblendps", "vblendvpd", "vblendvps", "vbroadcastf128", "vbroadcastf128", "vbroadcastsd", "vbroadcastss", "vbroadcastss", "vroundpd", "vroundps", "vcmppd", "vcmppd", "vcmpps", "vcmpps", "vcmpsd", "vcmpss", "vcvtdq2pd", "vcvtdq2ps", "vcvtpd2dq", "vcvtpd2ps", "vcvtps2dq", "vcvtps2pd", "vmovsd", "vmovd", "vmovss", "vcvttpd2dq", "vcvttps2dq", "vdivpd", "vdivps", "vdpps", "vextractf128", "vextractf128", "vextractf128", "vroundpd", "vroundps", "vhaddpd", "vhaddps", "vhsubpd", "vhsubps", "vinsertf128", "vinsertf128", "vinsertf128", "vlddqu", "vmovapd", "vmovaps", "vmovdqa", "vmovupd", "vmovups", "vmovdqu", "vmaskmovpd", "vmaskmovpd", "vmaskmovps", "vmaskmovps", "vmaskmovpd", "vmaskmovpd", "vmaskmovps", "vmaskmovps", "vmaxpd", "vmaxps", "vminpd", "vminps", "vmovddup", "vmovshdup", "vmovsldup", "vmovmskpd", "vmovmskps", "vmulpd", "vmulps", "vorpd", "vorps", "vpermilpd", "vpermilpd", "vpermilps", "vpermilps", "vperm2f128", "vperm2f128", "vperm2f128", "vpermilpd", "vpermilpd", "vpermilps", "vpermilps", "vrcpps", "vroundpd", "vroundps", "vrsqrtps", "vinsertf128", "vinsertf128", "vinsertf128", "vinsertf128", "vinsertf128", "vinsertf128", "vxorpd", "vxorps", "vpxor", "vshufpd", "vshufps", "vsqrtpd", "vsqrtps", "vmovapd", "vmovaps", "vmovdqa", "vmovupd", "vmovups", "vmovdqu", "vmovntpd", "vmovntps", "vmovntdq", "vsubpd", "vsubps", "vtestpd", "vtestps", "vptest", "vunpckhpd", "vunpckhps", "vunpcklpd", "vunpcklps", "vxorpd", "vxorps", "vzeroall", "vzeroupper"),
     "SSE4.2:": ("pcmpestri", "pcmpestri", "pcmpestri", "pcmpestrm", "pcmpestri", "pcmpestri", "pcmpestri", "pcmpgtq", "pcmpistri", "pcmpistri", "pcmpistri", "pcmpistrm", "pcmpistri", "pcmpistri", "pcmpistri", "crc32", "crc32", "crc32", "crc32"),
     "SSE4.1": ("pblendw", "blendpd", "blendps", "pblendvb", "blendvpd", "blendvps", "roundpd", "roundps", "roundsd", "roundss", "pcmpeqq", "pmovsxwd", "pmovsxwq", "pmovsxdq", "pmovsxbw", "pmovsxbd", "pmovsxbq", "pmovzxwd", "pmovzxwq", "pmovzxdq", "pmovzxbw", "pmovzxbd", "pmovzxbq", "dppd", "dpps", "pextrd", "pextrq", "pextrb", "extractps", "roundpd", "roundps", "roundsd", "roundss", "pinsrd", "pinsrq", "pinsrb", "insertps", "pmaxsd", "pmaxsb", "pmaxuw", "pmaxud", "pminsd", "pminsb", "pminuw", "pminud", "phminposuw", "mpsadbw", "pmuldq", "pmulld", "packusdw", "roundpd", "roundps", "roundsd", "roundss", "movntdqa", "ptest", "ptest", "ptest", "ptest", "ptest"),
     "SSSE3": ("pabsw", "pabsd", "pabsb", "pabsw", "pabsd", "pabsb", "palignr", "palignr", "phaddw", "phaddd", "phaddw", "phaddw", "phaddsw", "phaddsw", "phsubw", "phsubd", "phsubw", "phsubd", "phsubsw", "phsubsw", "pmaddubsw", "pmaddubsw", "pmulhrsw", "pmulhrsw", "pshufb", "pshufb", "psignw", "psignd", "psignb", "psignw", "psignd", "psignb"),
@@ -35,12 +35,17 @@ def run_llvm_objdump(file, symbol, cpu):
 
     return subprocess.run(args, capture_output=True, text=True)
 
+def preprocess_assembly(str):
+    return set(re.findall("^\s+(\S+)", str, flags=re.MULTILINE))
+
 def check_cpu_features(assembly_str):
-    found_features = []
+    found_features = {}
+    instructions_lookup = preprocess_assembly(assembly_str)
 
     for name, instructions in CPU_FEATURES_TABLE.items():
-        if re.search(fr"^\s+(?:{'|'.join(instructions).lower()})", assembly_str, flags=re.ASCII | re.MULTILINE):
-            found_features.append(name)
+        for instruction in instructions:
+            if instruction.lower() in instructions_lookup:
+                found_features.setdefault(name, []).append(instruction.lower())
 
     return found_features
 
@@ -68,7 +73,7 @@ def main():
         eprint(f"Tool '{LLVM_OBJDUMP}' not found")
         sys.exit(1)
 
-    clean_assembly = re.sub(r"^.+\(.+\):\s.+\n", " ", objdump_output.stdout, flags=re.ASCII | re.MULTILINE)
+    clean_assembly = re.sub(r"(^.+\(.+\):\s.+\n)|(^\n)|(^Disassembly of section .+\n)|(^<.+>:\n)", "", objdump_output.stdout, flags=re.ASCII | re.MULTILINE)
     if args.symbol not in clean_assembly:
         eprint(f"Can't find the '{args.symbol}' symbol in the file '{args.file}'.")
         sys.exit(1)
@@ -77,8 +82,9 @@ def main():
     if len(found_cpu_features) == 0:
         print("None of the known instruction sets were found")
     else:
-        print(f"Found these CPU features: {', '.join(found_cpu_features)}")
-    
+        print("Found these CPU features:")
+        for name, instructions in found_cpu_features.items():
+            print(f"{name}: {', '.join(instructions)}")
 
 if __name__ == "__main__":
     main()
