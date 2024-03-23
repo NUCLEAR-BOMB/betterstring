6c36cd45509889acbfc04399864e4d3e74617aee
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2024-03-24 01:17:11 +0500

    Refactor implementation of printing available cpu features

--- test/cpu_dispatch.cpp
+++ test/cpu_dispatch.cpp
@@ -9,21 +9,28 @@
 #include <betterstring/string_view.hpp>
 #include <betterstring/detail/cpu_features.hpp>
 
-TEST_CASE("cpu features", "[cpu_features]") {
+namespace {
+
+void print_current_cpu_features() {
     using bs::detail::cpu_features;
     const auto& value = cpu_features.value;
 
-    std::vector<bs::string_view> cpu_features_list;
-    // NOLINTBEGIN(readability-implicit-bool-conversion)
-    if (value & cpu_features.AVX2) { cpu_features_list.emplace_back("AVX2"); }
-    if (value & cpu_features.BMI2) { cpu_features_list.emplace_back("BMI2"); }
-    if (value & cpu_features.POPCNT) { cpu_features_list.emplace_back("POPCNT"); }
-    // NOLINTEND(readability-implicit-bool-conversion)
+    const auto print_feature = [](const char* name) {
+        std::fputs(name, stdout);
+        std::putchar(' ');
+    };
 
     std::fputs("\nCurrent processor supports these extensions: ", stdout);
-    for (auto feature_name : cpu_features_list) {
-        std::fwrite(feature_name.data(), sizeof(char), feature_name.size(), stdout);
-        std::fputs(" ", stdout);
-    }
-    std::fputs("\n", stdout);
+    // NOLINTBEGIN(readability-implicit-bool-conversion)
+    if (value & cpu_features.AVX2) { print_feature("AVX2"); }
+    if (value & cpu_features.BMI2) { print_feature("BMI2"); }
+    if (value & cpu_features.POPCNT) { print_feature("POPCNT"); }
+    // NOLINTEND(readability-implicit-bool-conversion)
+    std::putchar('\n');
+}
+
+TEST_CASE("cpu features", "[cpu_features]") {
+    print_current_cpu_features();
+}
+
 }
