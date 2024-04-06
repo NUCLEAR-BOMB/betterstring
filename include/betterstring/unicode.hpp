153fd9ec4b77c29c6947adec76cd194ff18f66f3
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2024-04-07 02:34:24 +0500

    Add `bs::utf_traits` to 'betterstring/unicode.hpp' header

---
+++ include/betterstring/unicode.hpp
@@ -0,0 +1,78 @@
+
+// Copyright 2024.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <betterstring/detail/preprocessor.hpp>
+#include <cstddef>
+#include <cstdint>
+
+namespace bs {
+
+#if BS_HAS_CHAR8_T
+enum class u8char_t : char8_t {};
+#else
+enum class u8char_t : unsigned char {};
+#endif
+
+enum class utf_error : uint32_t {
+    ok = 0,
+    bad_lead = 1,
+
+};
+
+template<class T>
+class utf_result {
+public:
+    using value_type = T;
+    using error_type = utf_error;
+
+    constexpr utf_result(const utf_error err) noexcept : val{0}, err{err} {}
+    constexpr utf_result(const T result) noexcept : val{result}, err{utf_error::ok} {}
+
+    constexpr T value() const noexcept {
+        BS_VERIFY(err == utf_error::ok, "No valid utf result");
+        return val;
+    }
+    constexpr utf_error error() const noexcept {
+        return err;
+    }
+
+    constexpr bool has_error() const noexcept { return err != utf_error::ok; }
+    explicit constexpr operator bool() const noexcept { return err == utf_error::ok; }
+
+    friend constexpr bool operator==(const utf_result left, const utf_result right) noexcept {
+        if (left.err == right.err) { return true; }
+        return left.val == right.val;
+    }
+    friend constexpr bool operator!=(const utf_result left, const utf_result right) noexcept {
+        return !(left == right);
+    }
+
+private:
+    T val;
+    utf_error err;
+};
+
+template<class T>
+class utf_traits;
+
+template<>
+class utf_traits<u8char_t> {
+public:
+    using size_type = std::size_t;
+
+    static constexpr utf_result<uint8_t> length(const u8char_t ch) noexcept {
+        if ((uint8_t(ch) & 0b1000'0000) == 0b0000'0000) { return 1; }
+        if ((uint8_t(ch) & 0b1110'0000) == 0b1100'0000) { return 2; }
+        if ((uint8_t(ch) & 0b1111'0000) == 0b1110'0000) { return 3; }
+        if ((uint8_t(ch) & 0b1111'1000) == 0b1111'0000) { return 3; }
+        return utf_error::bad_lead;
+    }
+};
+
+}
+
+
---
+++ test/unicode.cpp
@@ -0,0 +1,37 @@
+
+// Copyright 2024.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <betterstring/unicode.hpp>
+
+namespace {
+
+using bs::u8char_t;
+
+using utf8_traits = bs::utf_traits<bs::u8char_t>;
+
+TEST_CASE("utf8 length", "[unicode]") {
+    CHECK(utf8_traits::length(u8char_t{127}) == 1);
+    CHECK(utf8_traits::length(u8char_t{'\x0'}) == 1);
+    CHECK(utf8_traits::length(u8char_t{'\x0'}) == 1);
+
+    CHECK(utf8_traits::length(u8char_t{0b1100'0000}) == 2);
+    CHECK(utf8_traits::length(u8char_t{0b1101'1111}) == 2);
+
+    CHECK(utf8_traits::length(u8char_t{0b1110'0000}) == 3);
+    CHECK(utf8_traits::length(u8char_t{0b1110'1111}) == 3);
+
+    CHECK(utf8_traits::length(u8char_t{0b1111'0000}) == 4);
+    CHECK(utf8_traits::length(u8char_t{0b1111'0111}) == 4);
+
+    CHECK(utf8_traits::length(u8char_t{0b1111'1000}) == bs::utf_error::bad_lead);
+    CHECK(utf8_traits::length(u8char_t{0b1111'1100}) == bs::utf_error::bad_lead);
+    CHECK(utf8_traits::length(u8char_t{0b1111'1110}) == bs::utf_error::bad_lead);
+    CHECK(utf8_traits::length(u8char_t{0b1111'1111}) == bs::utf_error::bad_lead);
+    CHECK(utf8_traits::length(u8char_t{0b1000'0000}) == bs::utf_error::bad_lead);
+}
+
+}
